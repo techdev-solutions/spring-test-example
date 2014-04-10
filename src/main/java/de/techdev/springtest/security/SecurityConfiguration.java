a7b079281d6369273165d346c387a79d53321fe2
Moritz Schulze <moritz.schulze@gmail.com>
2014-04-10 15:08:51 +0200

    Disabled CSRF for the sake of simplicity.

--- src/main/java/de/techdev/springtest/security/SecurityConfiguration.java
+++ src/main/java/de/techdev/springtest/security/SecurityConfiguration.java
@@ -40,6 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
+        http.csrf().disable();
     }
 
     @Bean
