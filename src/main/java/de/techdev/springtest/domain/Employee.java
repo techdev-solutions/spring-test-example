5fca907058b8bb6b5c2d8b4a913af123ce7943b7
Moritz Schulze <moritz.schulze@gmail.com>
2014-04-10 14:48:15 +0200

    Added salary field to employee.

--- src/main/java/de/techdev/springtest/domain/Employee.java
+++ src/main/java/de/techdev/springtest/domain/Employee.java
@@ -17,6 +17,8 @@ public class Employee {
 
     private String name;
 
+    private Integer salary;
+
     public Long getId() {
         return id;
     }
@@ -31,5 +33,13 @@ public class Employee {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
-CREATE TABLE employee (id INTEGER, name VARCHAR);
-INSERT INTO employee (id, name) VALUES (0, 'Moritz');
+CREATE TABLE employee (id INTEGER, name VARCHAR, salary INTEGER);
+INSERT INTO employee (id, name, salary) VALUES (0, 'Jon', 100000);
+INSERT INTO employee (id, name, salary) VALUES (1, 'Moritz', 75000);
